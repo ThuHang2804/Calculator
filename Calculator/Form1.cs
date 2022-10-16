2be97e743b9c5151c0f94c2a23e0ea98ed0151bd
ThuHang2804 <2054052018hang@ou.edu.vn>
2022-10-16 21:55:28 +0700

    Them su kien button cong

--- Calculator/Form1.cs
+++ Calculator/Form1.cs
@@ -6,5 +6,16 @@ namespace Calculator
         {
             InitializeComponent();
         }
+
+        private void btCong_Click(object sender, EventArgs e)
+        {
+            int a, b, ketQua;
+            a = int.Parse(txtSoa.Text);
+            b = int.Parse(txtSob.Text);
+            ketQua = a + b;
+            txtkq.Text = ketQua.ToString();
+
+
+        }
     }
 }
